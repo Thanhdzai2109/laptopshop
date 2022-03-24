e08a2687fe8beee34b5132aff7ed9d771ce13418
thanhdzai2109@gmail.com <thanhdzai2109@gmail.com>
2022-03-24 16:56:45 +0700

    first commit

---
+++ src/main/java/com/laptopshop/AssignmentSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSpringMvcApplication.class, args);
+	}
+
+}
